4fd5f0903d6d456e71d2f70b385d66d72b648a26
Anatoly Makshantsev <a.makshantsev@gmail.com>
2017-05-13 13:49:11 +0400

    Created MainActivity class and layout

---
+++ MyOffice/app/src/main/java/com/tm_studio/myoffice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tm_studio.myoffice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by asd on 13.05.2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
