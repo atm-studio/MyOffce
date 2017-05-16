95921a695e8fb50f5c8b3d54c5b992aab00e1284
Anatoly Makshantsev <a.makshantsev@gmail.com>
2017-05-16 16:41:42 +0400

    Added Toolbar and Search button

--- MyOffice/app/src/main/java/com/tm_studio/myoffice/MainActivity.java
+++ MyOffice/app/src/main/java/com/tm_studio/myoffice/MainActivity.java
@@ -3,16 +3,31 @@ package com.tm_studio.myoffice;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
-/**
- * Created by asd on 13.05.2017.
- */
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar ();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick (MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
